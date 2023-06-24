fc4bb2cdbc1dde58f3bc9d03bc2326e324adcc9c
anUnnecessary <raihanshamil33@gmail.com>
2023-06-24 11:53:34 +0600

    array added

---
+++ user-define-types/array-types.ts
@@ -0,0 +1,18 @@
+// let userName: string[];
+let userName: Array<string>;
+
+
+userName = ["abdullah", "raihan"];
+
+console.log(userName);
+
+
+
+let multiplyTypes : (string|number)[];
+
+
+multiplyTypes = [1 , "name"];
+
+
+userName.sort();
+console.log(userName);
