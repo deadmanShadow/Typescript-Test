d531bcfd7ac861cc7a22e23236cf8e2a42b79952
anUnnecessary <raihanshamil33@gmail.com>
2023-06-24 11:47:31 +0600

    union types

---
+++ user-define-types/union-types.ts
@@ -0,0 +1,14 @@
+let userId : (string | number | boolean);
+
+userId = "1041";
+userId = 1556;
+
+
+
+function displayUSerInfo(userId){
+    console.log(userId);
+}
+
+
+displayUSerInfo("101");
+displayUSerInfo(155);
