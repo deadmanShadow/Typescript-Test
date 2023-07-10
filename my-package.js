7dc2ac55a78dee7bf4a4d3245b2dbcae5eaf0c16
anUnnecessary <raihanshamil33@gmail.com>
2023-07-10 11:54:20 +0600

    custom type added

---
+++ my-package.js
@@ -0,0 +1,21 @@
+const myExtension = {
+    autoImpact,
+    autoRenameTag,
+    betterComments,
+    codeSpellChecker,
+    codeSnap,
+    ES7__react,
+    eslint,
+    JavaScript_es6_codeSnippets,
+    LivePreview,
+    LiveServer,
+    MaterialIconTheme,
+    monogoDBforVSCODE,
+    npmIntellisense,
+    PathIntellisense,
+    PowerMode,
+    ReactExtensionPack,
+    ReactJSCODESnippets,
+    searchNodeModules,
+    TailwindCSSIntellisense
+}
---
+++ user-define-types/object-type.ts
@@ -0,0 +1,3 @@
+ let user1 = {userName: "shamil", userID: 101};
+ user1 = {userName: "shamil", userID: 101};
+ console.log(user1);
